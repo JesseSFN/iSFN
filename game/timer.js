3ae928d485734b143320b3864e571036012305a9
JesseSFN <jesse@scoutingfn.nl>
2025-10-27 13:07:34 +0100

    OPSCHIETEN toegevoegd

---
+++ game/timer.js
@@ -0,0 +1,20 @@
+// timer.js
+export function startMotivatieTimer(intervalMs = 60000) {
+  const audio = new Audio('../Sounds/OPSCHIETEN.wav'); // je mp3
+  audio.volume = 1; // max volume
+  audio.preload = 'auto';
+
+  function playAudio() {
+    audio.currentTime = 0;
+    audio.play().catch(e => console.warn('Autoplay geblokkeerd:', e));
+  }
+
+  // eerste keer na interval
+  setTimeout(() => {
+    playAudio();
+    setInterval(playAudio, intervalMs);
+  }, intervalMs);
+}
+
+// automatisch starten bij import
+startMotivatieTimer(); 
