37bbc35403d1478e4a02212034cb140cc821a31e
JesseSFN <jesse@scoutingfn.nl>
2025-10-28 15:13:37 +0100

    tets

---
+++ game/test-loc.js
@@ -0,0 +1,11 @@
+const locations = [
+      {
+        name: "Lego",
+        hint: "In dit land worden de meeste banden gemaakt",
+        ans: "Denemarken",
+        mediSa: "",
+        lat: 56.02453877107302,
+        lng: 10.521098060541842,
+        radius: 300 // // ongeveer 300 km
+      }, 
+];
