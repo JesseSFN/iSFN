ce962eaedb9fca88e7ae376529b618351dcd2c38
JesseSFN <jesse@scoutingfn.nl>
2025-11-03 13:28:32 +0100

    test loc

--- game/test-loc.js
+++ game/test-loc.js
@@ -1,11 +1,30 @@
 const locations = [
       {
-        name: "Lego",
-        hint: "In dit land worden de meeste banden gemaakt",
+        name: "Dit is een test vraag",
+        hint: "Ga naar Denemarken",
         ans: "Denemarken",
         mediSa: "",
         lat: 56.02453877107302,
         lng: 10.521098060541842,
         radius: 300 // // ongeveer 300 km
-      }, 
+      },
+      {
+        name: "Dit is een test vraag",
+        hint: "Ga naar Nederland",
+        ans: "Nederland",
+        mediSa: "",
+        lat: 51.5244032,
+        lng: 4.4761088,
+        radius: 300 // // ongeveer 300 km
+      },
+      {
+        name: "Dit is een test vraag",
+        hint: "Ga naar Malta",
+        ans: "Nederland",
+        mediSa: "",
+        lat: 35.885868,
+        lng: 14.4027236,
+        radius: 300 // // ongeveer 300 km
+      },
+      
 ];
