68325e4b0d257a224b90aa05799edfd06e376c49
JesseSFN <jesse@scoutingfn.nl>
2025-11-08 14:30:50 +0100

    dudjvj

--- game/script.js
+++ game/script.js
@@ -2,8 +2,10 @@
 let currentLocation = null;
 const map = L.map('map').setView([20, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a> | © OpenStreetMap contributors',
+  subdomains: 'abcd',
+  maxZoom: 19
 }).addTo(map);
 
 const locationsList = document.getElementById('locations-list').getElementsByTagName('ul')[0];
