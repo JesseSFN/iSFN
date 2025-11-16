b65506275d9a04924a12e4d37037f0d85145554f
JesseSFN <jesse@scoutingfn.nl>
2025-11-16 22:35:00 +0100

    vv

--- game/locations.js
+++ game/locations.js
@@ -393,7 +393,7 @@ const locations = [
         media: "",
         lat: 36.998993167946544,
         lng: -109.04520476736661,
-        radius: 0.1 // ongeveer 100 meter
+        radius: 0.03 // ongeveer 30 meter
       },
 
 ];
